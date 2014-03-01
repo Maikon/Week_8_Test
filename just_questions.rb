012cabaf8212d67fe4237b92ac357f808813d10e
Makis Otman <makisotman@gmail.com>
2014-03-01 15:36:01 +0000

    solved 5 more

--- just_questions.rb
+++ just_questions.rb
@@ -22,21 +22,25 @@ def every_possible_pairing_of_students(arg)
   arg.combination(2)
 end
 
-# discard the first 3 elements of an array,
-# e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
+def all_elements_except_first_3(arg)
+  arg[3, arg.length]
+end
 
-# add an element to the beginning of an array
+def add_element_to_beginning_of_array(array, element)
+  array.unshift(element)
+end
 
-# sort an array of words by their last letter, e.g.
-# ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
+def array_sort_by_last_letter_of_word(arg)
+  arg.sort_by { |x| x[-1] }
+end
 
-# cut strings in half, and return the first half, e.g.
-# 'banana' becomes 'ban'. If the string is an odd number of letters
-# round up - so 'apple' becomes 'app'
+def get_first_half_of_string(arg)
+  arg[0, ((arg.length / 2.0).round)]
+end
 
-# turn a positive integer into a negative integer. A negative integer
-# stays negative
-
+def make_numbers_negative(arg)
+  arg > 0 ? -arg : arg
+end
 # turn an array of numbers into two arrays of numbers, one an array of
 # even numbers, the other an array of odd numbers
 # even numbers come first
