465e050214076121b2d94c2e7d8befc98ce1c665
Makis Otman <makisotman@gmail.com>
2014-03-02 13:47:44 +0000

    few more done, 3 more left plus some refactoring

--- just_questions.rb
+++ just_questions.rb
@@ -1,90 +1,90 @@
-def select_elements_starting_with_a(arg)
-  arg.select { |x| x if x[0] == 'a' }
+def select_elements_starting_with_a(array)
+  array.select { |x| x if x[0] == 'a' }
 end
 
-def select_elements_starting_with_vowel(arg)
-  arg.select! { |x| x if x[0] =~ /[aeiou]/ }
+def select_elements_starting_with_vowel(array)
+  array.select! { |x| x if x[0] =~ /[aeiou]/ }
 end
 
-def remove_nils_from_array(arg)
-  arg.compact!
+def remove_nils_from_array(array)
+  array.compact!
 end
 
-def remove_nils_and_false_from_array(arg)
-  arg.reject { |x| !x }
+def remove_nils_and_false_from_array(array)
+  array.reject { |x| !x }
 end
 
-def reverse_every_element_in_array(arg)
-  arg.map! { |x| x.reverse }
+def reverse_every_element_in_array(array)
+  array.map! { |x| x.reverse }
 end
 
 def every_possible_pairing_of_students(arg)
   arg.combination(2)
 end
 
-def all_elements_except_first_3(arg)
-  arg[3, arg.length]
+def all_elements_except_first_3(array)
+  array[3, array.length]
 end
 
 def add_element_to_beginning_of_array(array, element)
   array.unshift(element)
 end
 
-def array_sort_by_last_letter_of_word(arg)
-  arg.sort_by { |x| x[-1] }
+def array_sort_by_last_letter_of_word(array)
+  array.sort_by { |x| x[-1] }
 end
 
-def get_first_half_of_string(arg)
-  arg[0, ((arg.length / 2.0).round)]
+def get_first_half_of_string(string)
+  string[0, ((string.length / 2.0).round)]
 end
 
-def make_numbers_negative(arg)
-  arg > 0 ? -arg : arg
+def make_numbers_negative(num)
+  num > 0 ? -num : num
 end
 
-def separate_array_into_even_and_odd_numbers(arg)
-  evens, odds = arg.select { |num| num.even? },
-                arg.select { |num| num.odd?  }
+def separate_array_into_even_and_odd_numbers(array)
+  evens, odds = array.select { |num| num.even? },
+                array.select { |num| num.odd?  }
 end
 
-def number_of_elements_that_are_palindromes(arg)
-  arg.select { |word| word == word.reverse }.size
+def number_of_elements_that_are_palindromes(array)
+  array.select { |word| word == word.reverse }.size
 end
 
-def shortest_word_in_array(arg)
-  arg.reduce { |a, e| a.length < e.length ? a : e }
+def shortest_word_in_array(array)
+  array.reduce { |a, e| a.length < e.length ? a : e }
 end
 
-def longest_word_in_array(arg)
-  arg.reduce { |a, e| a.length > e.length ? a : e }
+def longest_word_in_array(array)
+  array.reduce { |a, e| a.length > e.length ? a : e }
 end
 
-def total_of_array(arg)
-  arg.reduce(:+)
+def total_of_array(array)
+  array.reduce(:+)
 end
 
-def double_array(arg)
-  arg * 2
+def double_array(array)
+  array * 2
 end
 
-def turn_symbol_into_string(arg)
-  arg.to_s
+def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
-def average_of_array(arg)
-  (arg.reduce(:+).to_f / arg.length).round
+def average_of_array(array)
+  (array.reduce(:+).to_f / array.length).round
 end
 
-def get_elements_until_greater_than_five(arg)
-  arg.take_while { |x| x <= 5 }
+def get_elements_until_greater_than_five(array)
+  array.take_while { |x| x <= 5 }
 end
 
-def convert_array_to_a_hash(arg)
-  Hash[*arg]
+def convert_array_to_a_hash(array)
+  Hash[*array]
 end
 
-def get_all_letters_in_array_of_words(arg)
-  arg.map { |word| word.split(//) }.flatten.uniq.sort
+def get_all_letters_in_array_of_words(array)
+  array.map { |word| word.split(//) }.flatten.uniq.sort
 end
 
 def swap_keys_and_values_in_a_hash(hash)
@@ -114,35 +114,54 @@ end
 def get_domain_name_from_email_address(email)
   email[/@(\w+)/, 1]
 end
-# capitalize the first letter in each word of a string,
-#  except 'a', 'and' and 'the'
-# *unless* they come at the start of the start of the string, e.g.
-# 'the lion the witch and the wardrobe' becomes
-# 'The Lion the Witch and the Wardrobe'
 
-# return true if a string contains any special characters
-# where 'special character' means anything apart from the letters
-# a-z (uppercase and lower) or numbers
+def titleize_a_string(string)
+  misc_words = %w(the and a)
+  words = string.split(' ').map do |word|
+    misc_words.include?(word) ? word : word.capitalize!
+  end
+  words[0].capitalize!
+  words.join(' ')
+end
 
-# get the upper limit of a range. e.g. for the range 1..20, you
-# should return 20
+def check_a_string_for_special_characters(string)
+  string[/[^\d^a-zA-Z]/]
+end
 
-# should return true for a 3 dot range like 1...20, false for a
-# normal 2 dot range
+def get_upper_limit_of(range)
+  range.to_a[-1]
+end
 
-# get the square root of a number
+def is_a_3_dot_range?(range)
+  range.exclude_end?
+end
 
-# count the number of words in a file
+def square_root_of(num)
+  Math.sqrt(num)
+end
 
-# --- tougher ones ---
+def word_count_a_file(file)
+  IO.binread(file).split.count
+end
 
-# call an arbitrary method from a string. so if I
-# called call_method_from_string('foobar')
-# the method foobar should be invoked
+def call_method_from_string(a_string)
+  a_string = lambda { raise NameError }
+  a_string.call
+end
 
-# return true if the date is a uk bank holiday for 2014
-# the list of bank holidays is here:
-# https://www.gov.uk/bank-holidays
+def is_a_2014_bank_holiday?(date)
+  chosen_date = date
+  holidays = [
+    Time.new(2014, 4, 18),
+    Time.new(2014, 4, 21),
+    Time.new(2014, 5, 5),
+    Time.new(2014, 5, 26),
+    Time.new(2014, 8, 25),
+    Time.new(2014, 12, 25),
+    Time.new(2014, 12, 26),
+  ]
+  holidays.include?(chosen_date)
+end
 
 # given your birthday this year, this method tells you
 # the next year when your birthday will fall on a friday
@@ -159,9 +178,16 @@ end
 # go from 1 to 100
 # (there's no RSpec test for this one)
 
-# print the lyrics of the song 99 bottles of beer on the wall
-# http://www.99-bottles-of-beer.net/lyrics.html
-# make sure you use the singular when you have one bottle of
-# beer on the wall, and print 'no more bottles of beer on the wall'
-# at the end.
-# (there's no RSpec test for this one)
+def the_99_bottles_of_beer_song
+  99.downto(0) do |x|
+    if x > 0
+      bottle_form = x > 1 ? 'bottles' : 'bottle'
+      expression = x - 1 > 0 ? (x - 1).to_s + ' ' + bottle_form : 'no more bottles'
+      puts "#{x} #{bottle_form} on beer on the wall, #{x} #{bottle_form} of beer."
+      puts "Take one down and pass it around , #{expression} on the wall."
+    else
+      puts 'No more bottles of beer on the wall, no more bottles of beer.'
+      puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+    end
+  end
+end
